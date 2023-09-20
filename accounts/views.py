89734c61a7412a3f030937d448b899a1fe852889
Adnane <adnmou95@gmail.com>
2023-09-21 00:02:18 +0100

    User authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/admin')
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return render(request, "accounts/login.html", {'error': 'invalid username or password'})
+        login(request, user)
+        # Will be replaced by a redirection link for bookings
+        return redirect('/admin')
+    return render(request, "accounts/login.html", {})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/login')
+    return render(request, "accounts/logout.html", {})
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import login_view, logout_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view),
+    path('logout/', logout_view),
 ]
